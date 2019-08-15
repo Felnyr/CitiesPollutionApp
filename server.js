1323df451be4f3b94819d673650764e0768ca7fd
Felnyr <pawlello@gmail.com>
2019-08-15 23:40:33 +0200

    minor server change

--- server.js
+++ server.js
@@ -3,6 +3,5 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000
 app.use(express.static('public'))
-// app.use(express.static('C:/Users/Nowy użytkownik/Desktop/github - refactor/FrontEndTask/dist/bundle.js'))
 app.listen(PORT)
 
